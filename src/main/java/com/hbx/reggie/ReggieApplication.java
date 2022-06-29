c2ec32fa63c130c887a0496bd2c7f763f0c3b306
ham <1499091922@qq.com>
2022-06-29 14:50:52 +0800

    day-1-1.0----environment

---
+++ src/main/java/com/hbx/reggie/ReggieApplication.java
@@ -0,0 +1,18 @@
+package com.hbx.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 黄柏轩
+ * @version 1.0
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("瑞吉外卖项目启动");
+    }
+}
---
+++ src/main/java/com/hbx/reggie/config/staticConfig.java
@@ -0,0 +1,20 @@
+package com.hbx.reggie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 黄柏轩
+ * @version 1.0
+ */
+@Configuration
+public class staticConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+//
+//    }
+
+
+}
